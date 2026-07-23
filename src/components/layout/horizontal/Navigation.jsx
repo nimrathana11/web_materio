00b6c1e73dcade18a282d7562ec823e53482c0fc
Nim Rathana <126838139+NimRathana@users.noreply.github.com>
2026-07-23 16:41:56 +0700

    commit

--- src/components/layout/horizontal/Navigation.jsx
+++ src/components/layout/horizontal/Navigation.jsx
@@ -3,32 +3,16 @@
 // Component & Core Hook Imports
 import HorizontalMenu from './HorizontalMenu'
 import { useSettings } from '@core/hooks/useSettings'
-import themeConfig from '@configs/themeConfig'
-import useHorizontalNav from "@menu/hooks/useHorizontalNav";
 
 const Navigation = () => {
   // Hooks
   const { settings } = useSettings()
-  const { isBreakpointReached } = useHorizontalNav();
 
   // If layout is vertical, this component is not rendered
   if (settings.layout !== 'horizontal') return null
-  if (isBreakpointReached) {
-    return null;
-  }
   
   return (
-    <div 
-      style={{
-        paddingInline: `${themeConfig.layoutPadding}px`,
-        transition: 'padding 0.3s ease',
-        minHeight: 'calc(var(--header-height) - 5px)',
-        display: 'flex',
-        alignItems: 'center',
-      }}
-    >
-      <HorizontalMenu />
-    </div>
+    <HorizontalMenu />
   )
 }
 
